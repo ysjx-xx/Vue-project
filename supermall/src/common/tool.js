4ecc80b37c52cdbeb0d17d2e8d4c5d9bef82739c
XiaXi <xiaxi@19422.com>
2021-07-08 17:59:55 +0800

    delete .supermall dir

--- supermall/src/common/tool.js
+++
@@ -1,9 +0,0 @@
-export function debounce(func, delay) {
-    let timer = 0
-    return function (...args) {
-      if (timer) clearTimeout(timer);
-      timer = setTimeout(() => {
-        func.apply(this, args);
-      }, delay);
-    }
-}
